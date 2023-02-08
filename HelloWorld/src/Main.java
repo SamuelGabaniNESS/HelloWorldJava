77381dcdc85920b5d28cf53e52bd8fa721b92030
SamuelGabaniNESS <124255165+SamuelGabaniNESS@users.noreply.github.com>
2023-02-08 10:58:16 +0100

    other operations

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,11 +2,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int num1,num2,result = 0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Write first number: ");
-        int num1 = sc.nextInt();
+        num1 = sc.nextInt();
         System.out.print("Write second number: ");
-        int num2 = sc.nextInt();
-        System.out.println("Sum of first and second number is "+(num1+num2));
+        num2 = sc.nextInt();
+        System.out.println("Operation (+,-,*,/):");
+        String operation = sc.next();
+        switch (operation){
+            case "+":
+                result = num1+num2;
+                break;
+            case "-":
+                result = num1-num2;
+                break;
+            case "*":
+                result = num1*num2;
+                break;
+            case "/":
+                if(num2!=0) {
+                    result = num1 / num2;
+                }else{
+                    System.out.println("Can't divide number with 0.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Wrong operation");
+                return;
+        }
+        System.out.println("Sum of first and second number is "+result);
     }
 }
