88713fbfb153a662df39a41cde1ecf65bce81fa2
SamuelGabaniNESS <124255165+SamuelGabaniNESS@users.noreply.github.com>
2023-02-08 12:13:46 +0100

    oprava chyb a cistenie kodu

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,37 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        boolean running = true;
         int num1,num2,result = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Write first number: ");
-        num1 = sc.nextInt();
-        System.out.print("Write second number: ");
-        num2 = sc.nextInt();
-        System.out.println("Operation (+,-,*,/):");
-        String operation = sc.next();
-        switch (operation){
-            case "+":
-                result = num1+num2;
-                break;
-            case "-":
-                result = num1-num2;
-                break;
-            case "*":
-                result = num1*num2;
-                break;
-            case "/":
-                if(num2!=0) {
-                    result = num1 / num2;
-                }else{
-                    System.out.println("Can't divide number with 0.");
+        while(running) {
+            num1 = getNumber();
+            num2 = getNumber();
+            System.out.println("Operation (+,-,*,/):");
+            String operation = sc.nextLine();
+            switch (operation) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Can't divide number with 0.");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Wrong operation");
                     return;
-                }
-                break;
-            default:
-                System.out.println("Wrong operation");
-                return;
+            }
+            System.out.println("Sum of first and second number is " + result);
+            System.out.println("Do you wish to continue? Write(y/n)");
+            String answer = sc.nextLine();
+            if(answer.equals("n")){
+                running = false;
+            }
+            System.out.println("=".repeat(30));
         }
-        System.out.println("Sum of first and second number is "+result);
+    }
+
+    public static int getNumber(){
+        int num = 0;
+        boolean rightInput = false;
+        do {
+            System.out.print("Write a number: ");
+            try {
+                num = Integer.parseInt(sc.nextLine());
+                rightInput = false;
+            } catch (Exception e) {
+                System.out.print("\nNumber is not integer.");
+                rightInput = true;
+            }
+        }while(rightInput);
+        return num;
     }
 }
