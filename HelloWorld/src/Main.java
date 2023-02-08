c502e31b33eddd7984cc0ce5919f584accc6ece0
SamuelGabaniNESS <124255165+SamuelGabaniNESS@users.noreply.github.com>
2023-02-08 10:44:42 +0100

    sumNumber

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for(int i=0;i<10;i++){
-            System.out.println(i);
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Write first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Write second number: ");
+        int num2 = sc.nextInt();
+        System.out.println("Sum of first and second number is "+(num1+num2));
     }
 }
